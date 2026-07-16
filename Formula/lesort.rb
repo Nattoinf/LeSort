b407003afbe45eaecaeb715c8e95745f7dc9352c
Nattoinf <nattoinf151026@au.com>
2026-07-17 00:42:46 +0900

    fix: improve homebrew formula

--- Formula/lesort.rb
+++ Formula/lesort.rb
@@ -2,6 +2,7 @@ class Lesort < Formula
   desc "File organization analyzer and scoring tool"
   homepage "https://github.com/Nattoinf/LeSort"
   url "https://github.com/Nattoinf/LeSort/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "012556d2dd75db314c234c48b7438ea9dec1b73f7a1521303326b8af7a636272"
   license "MIT"
 
   depends_on "rust" => :build
@@ -12,6 +13,6 @@ class Lesort < Formula
   end
 
   test do
-    system "#{bin}/lesort", "--version"
+    system bin/"lesort", "--version"
   end
 end
