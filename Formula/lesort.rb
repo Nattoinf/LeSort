962d22c5142f7de4f3b971ca8a23b88b954049f2
Nattoinf <nattoinf151026@au.com>
2026-07-17 00:18:10 +0900

    fix: prepare homebrew installation

---
+++ Formula/lesort.rb
@@ -0,0 +1,17 @@
+class Lesort < Formula
+  desc "File organization analyzer and scoring tool"
+  homepage "https://github.com/Nattoinf/LeSort"
+  url "https://github.com/Nattoinf/LeSort/archive/refs/tags/v0.1.0.tar.gz"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release"
+    bin.install "target/release/lesort"
+  end
+
+  test do
+    system "#{bin}/lesort", "--version"
+  end
+end
